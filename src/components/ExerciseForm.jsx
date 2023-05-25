7341bafbed78f1af2bb7c94cf3fece9ba5fefd5e
Jonathan Villamizar <jvilla8a@gmail.com>
2023-05-25 02:07:04 -0500

    fixing picker

--- src/components/ExerciseForm.jsx
+++ src/components/ExerciseForm.jsx
@@ -1,45 +1,73 @@
-import React, { useState } from 'react';
-import { Formik } from 'formik';
-import SelectPicker from 'react-native-picker-select';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { View, Text, StyleSheet, TextInput, Button, Switch } from 'react-native';
+import React, { useState, useRef, useEffect } from "react";
+import { Formik } from "formik";
+import { Picker } from "@react-native-picker/picker";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  Switch,
+} from "react-native";
 
-import { EXERCISE_RECORD } from '../utils/forms';
-
+import { EXERCISE_RECORD } from "../utils/forms";
+// TODOS:
+// Usar formik hooks
+// Mejorar picker en ios
 const ExerciseForm = (props) => {
   const { initialValues, validationSchema } = EXERCISE_RECORD;
   const { handleFormSubmit, variation = [], setShowForm, isBlock } = props;
-  
+  const selectRef = useRef(null);
 
   const [weightKg, setWeightKg] = useState(true);
 
   const handleWeightKg = (formikValues) => {
     formikValues.weightKg = !weightKg;
     setWeightKg(!weightKg);
-  }
+  };
 
   return (
-    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={(values) => handleFormSubmit(values)}>
-      {({ handleChange, handleBlur, handleSubmit, values, errors, touched, isValid, dirty }) => (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => handleFormSubmit(values)}
+    >
+      {({
+        setFieldValue,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+        isValid,
+        dirty,
+      }) => (
         <View style={styles.inputsContainer}>
           <View style={styles.inputsRowContainer}>
             <View style={[styles.inputContainer, styles.weightInput]}>
               <Text style={styles.label}>Peso</Text>
               <TextInput
-                style={[styles.input, touched.weight && errors.weight && styles.errorInput]}
-                onChangeText={handleChange('weight')}
-                onBlur={handleBlur('weight')}
-                placeholder='Peso'
-                keyboardType='number-pad'
+                style={[
+                  styles.input,
+                  touched.weight && errors.weight && styles.errorInput,
+                ]}
+                onChangeText={handleChange("weight")}
+                onBlur={handleBlur("weight")}
+                placeholder="Peso"
+                keyboardType="number-pad"
                 value={values.weight}
               />
-              {touched.weight && errors.weight && <Text style={styles.errorMessage}>{errors.weight}</Text>}
+              {touched.weight && errors.weight && (
+                <Text style={styles.errorMessage}>{errors.weight}</Text>
+              )}
             </View>
             <View style={styles.inputsRow}>
               <Text style={styles.label}>Lb</Text>
               <Switch
-                trackColor={{false: '#424242', true: '#424242'}}
-                thumbColor={'#F5F5F5'}
+                trackColor={{ false: "#424242", true: "#424242" }}
+                thumbColor={"#F5F5F5"}
                 ios_backgroundColor="#424242"
                 onValueChange={() => handleWeightKg(values)}
                 value={weightKg}
@@ -50,52 +78,60 @@ const ExerciseForm = (props) => {
           <View style={styles.inputContainer}>
             <Text style={styles.label}>Repeticiones</Text>
             <TextInput
-              style={[styles.input, touched.reps && errors.reps && styles.errorInput]}
-              onChangeText={handleChange('reps')}
-              onBlur={handleBlur('reps')}
-              placeholder='Repeticiones'
-              keyboardType='number-pad'
+              style={[
+                styles.input,
+                touched.reps && errors.reps && styles.errorInput,
+              ]}
+              onChangeText={handleChange("reps")}
+              onBlur={handleBlur("reps")}
+              placeholder="Repeticiones"
+              keyboardType="number-pad"
               value={values.reps}
             />
-            {touched.reps && errors.reps && <Text style={styles.errorMessage}>{errors.reps}</Text>}
+            {touched.reps && errors.reps && (
+              <Text style={styles.errorMessage}>{errors.reps}</Text>
+            )}
           </View>
           {isBlock && (
             <>
               <View style={styles.inputContainer}>
                 <Text style={styles.label}>Variante</Text>
-                <View style={styles.input}>
-                  <SelectPicker
-                    value={values.variation}
-                    placeholder={{
-                      label: 'Elige una variante...',
-                      value: '',
-                    }}
-                    onValueChange={handleChange('variation')}
-                    useNativeAndroidPickerStyle={true}
-                    items={[
-                      ...variation.map((item) => ({
-                        label: item,
-                        // value: item.toLowerCase().replace(/\s+/g, ''),
-                        value: item,
-                        key: item.toLowerCase().replace(/\s+/g, '')
-                      })),
-                      { label: 'Otra variación', value: 'other' },
-                    ]}
+                <Picker
+                  ref={selectRef}
+                  selectedValue={values.variation}
+                  onValueChange={(value) => setFieldValue("variation", value)}
+                >
+                  <Picker.Item
+                    label="Elige una variación..."
+                    value={null}
+                    enabled={false}
                   />
-                </View>
+                  {[...variation].map((item) => (
+                    <Picker.Item
+                      label={item}
+                      value={item}
+                      key={item.toLowerCase().replace(/\s+/g, "")}
+                    />
+                  ))}
+                  <Picker.Item label="Otra variación" value="other" />
+                </Picker>
               </View>
-              {values.variation === 'other' && (
+              {values.variation === "other" && (
                 <View style={styles.inputContainer}>
                   <Text style={styles.label}>Otra variante</Text>
-                    <TextInput
-                      style={styles.input}
-                      onChangeText={handleChange('otherVariant')}
-                      onBlur={handleBlur('otherVariant')}
-                      placeholder='Repeticiones'
-                      keyboardType='default'
-                      value={values.otherVariant}
-                    />
-                  {touched.otherVariant && errors.otherVariant && <Text style={styles.errorMessage}>{errors.otherVariant}</Text>}
+                  <TextInput
+                    style={styles.input}
+                    onChangeText={handleChange("otherVariant")}
+                    onBlur={handleBlur("otherVariant")}
+                    placeholder="Repeticiones"
+                    keyboardType="default"
+                    value={values.otherVariant}
+                  />
+                  {touched.otherVariant && errors.otherVariant && (
+                    <Text style={styles.errorMessage}>
+                      {errors.otherVariant}
+                    </Text>
+                  )}
                 </View>
               )}
             </>
@@ -112,58 +148,67 @@ const ExerciseForm = (props) => {
             {touched.otherVariant && errors.otherVariant && <Text style={styles.errorMessage}>{errors.otherVariant}</Text>}
           </View> */}
           <View style={styles.buttonsContainer}>
-            <Button color='#4C00A4' onPress={handleSubmit} title="Agregar serie" disabled={!(isValid && dirty)} />
-            <Button color='#4C00A4' onPress={() => setShowForm(false)} title="Cancelar" />
+            <Button
+              color="#4C00A4"
+              onPress={handleSubmit}
+              title="Agregar serie"
+              disabled={!(isValid && dirty)}
+            />
+            <Button
+              color="#4C00A4"
+              onPress={() => setShowForm(false)}
+              title="Cancelar"
+            />
           </View>
         </View>
       )}
     </Formik>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   inputsContainer: {
     padding: 16,
   },
   inputsRowContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   inputsRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    width: '30%',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    width: "30%",
     marginLeft: 8,
   },
   inputContainer: {
-    marginBottom: 8
+    marginBottom: 8,
   },
   input: {
     height: 40,
-    justifyContent: 'center',
-    borderColor: '#DDD',
+    justifyContent: "center",
+    borderColor: "#DDD",
     borderWidth: 1,
     borderRadius: 4,
     paddingHorizontal: 8,
   },
   label: {
-    color: '#424242',
+    color: "#424242",
   },
   weightInput: {
-    width: '70%'
+    width: "70%",
   },
   errorMessage: {
-    color: 'red',
+    color: "red",
     fontSize: 10,
   },
   errorInput: {
-    borderColor: 'red',
+    borderColor: "red",
   },
   buttonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     marginVertical: 8,
   },
 });
